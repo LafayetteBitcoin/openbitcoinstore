9f456faf72c86a683e954c83ee2c876d70f6c579
Andreas M. Antonopoulos <andreas@antonopoulos.com>
2013-04-01 05:05:54 -0700

    config bitpay

--- config/public/wp-content/plugins/cart/bitpay/config.php
+++ config/public/wp-content/plugins/cart/bitpay/config.php
@@ -1,20 +1,20 @@
 <?php
 
 // ecwid settings
-$storeURL = 'http://spw-store.herokuapp.com/';  // example: 'http://www.example.com/ecwid/index.html'
+$storeURL = 'https://spw-store.herokuapp.com/';  // example: 'http://www.example.com/ecwid/index.html'
 $storeId = '2136144'; // found in your ecwid control panel, bottom-right
 
 // bitpay settings
 // url of bitpay folder on your server.  example: 'http://www.example.com/ecwid/bitpay/
-$bitpayURL = ''; 
+$bitpayURL = 'https://spw-store.herokuapp.com/wp-content/plugins/cart/bitpay/'; 
 // apiKey: create this at bitpay.com in your account settings and paste it here
-$apiKey = '';  // ex 'DNboT9fVNpW7usAuDNboT9fVNpW7usAu'
+$apiKey = '7bOSaT7QlxZ4LK3CWNPb6YKhJ9785fpMFXavuydZoVE';  // ex 'DNboT9fVNpW7usAuDNboT9fVNpW7usAu'
 // speed: Warning: on medium/low, customers will not see an order confirmation page.  
 $speed = 'high'; // can be 'high', 'medium' or 'low'.  See bitpay API doc for more details.
 
 //payment method settings
-$login = ''; // see README
-$hashValue = ''; // see README
+$login = 'ci2KMqD4wc9kQY3w'; // see README
+$hashValue = '32dG4trRspBpVo3V'; // see README
 
 // add trailing slash to url
 $bitpayURL = preg_replace('#([^\/])$#', '\1/', $bitpayURL);
